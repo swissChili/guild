78aac801c219d0f52d03f3b37da864d1ce96b00f
swissChili <sasha@fastmail.com>
2019-03-04 08:23:14 -0800

    Added removal of overwritten vars

--- src/Codegen.hs
+++ src/Codegen.hs
@@ -3,26 +3,29 @@ module Codegen where
 import Types
 import Data.List
 
-rev :: [a] -> [a]  
 rev = foldl l [] where l = \acc x -> x : acc
 
-prefix :: String -> String -> Bool
+prefix :: Eq a => [a] -> [a] -> Bool
 prefix [] _ = True
 prefix (_:_) [] = False
 prefix (x:xs) (y:ys) = x == y && prefix xs ys
 
 suffix x y = prefix (rev x) (rev y)
 
-genCompileCommands :: String -> [String] -> SourceType -> String
-genCompileCommands t s C
+varsEq (Recipe _ _ _) _ = False
+varsEq _ (Recipe _ _ _) = False
+varsEq (Variable k1 _) (Variable k2 _) = k1 == k2
+filterVars = nubBy varsEq
+
+genCompileCommands t C
   | suffix ".o" t = "$(CC) -c -o $@ $^ $(CFLAGS)"
   | suffix ".a" t = "$(AR) rcs $@ $^ $(ARFLAGS)"
   | otherwise     = "$(CC) -o $@ $^ $(CFLAGS)"
-genCompileCommands t s CPlusPlus
+genCompileCommands t CPlusPlus
   | suffix ".o" t = "$(CPPC) -c -o $@ $^ $(CPPFLAGS)"
   | suffix ".a" t = "$(AR) rcs $@ $^ $(ARFLAGS)"
   | otherwise     = "$(CPPC) -o $@ $^ $(CPPFLAGS)"
-genCompileCommands t s Unknown
+genCompileCommands t Unknown
   | suffix ".o" t = "echo could not figure out how to build $@ from $^"
   | suffix ".a" t = "$(AR) rcs $@ $^"
   | otherwise     = "echo idk"
@@ -35,7 +38,8 @@ genMakefileRec ((Variable name binding):xs) c = genMakefileRec xs gen
 genMakefileRec ((Recipe target source sourceType):xs) c = genMakefileRec xs gen
   where
     gen = c ++ target ++ " : " ++ sp ++ "\n\t" ++ commands ++ "\n"
-    commands = genCompileCommands target source sourceType
+    commands = genCompileCommands target sourceType
     sp = intercalate " " source
 
-genMakefile i = genMakefileRec i ""
+genMakefile i = genMakefileRec f ""
+  where f = rev $ filterVars $ rev i
